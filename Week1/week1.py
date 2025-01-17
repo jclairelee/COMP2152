4070d8e4bb294631d66bde07cc109e19f65e9e8d
Claire Lee <jclairelee@gmail.com>
2025-01-17 12:23:35 -0800

    first commit

---
+++ Week1/week1.py
@@ -0,0 +1,55 @@
+print ("hello, world!")
+print(3)
+a=5
+print(a)
+
+type(a)
+
+b="Michael"
+print(b)
+
+print(type(a))
+
+print(type(b))
+
+print(type(9, 0))
+
+print(type(True))
+
+c=3
+d=4
+e=5
+f=6
+
+g= c + d ** e/ f % e
+
+h=c - d / e ** f + d * c
+print(h)
+
+# Lists
+l=[1,2,3,5]
+
+print(l[3])
+
+n=[1,2,3,"five",6.0]
+
+print(n)
+
+age =5
+print("My age is:", age)
+st = "I am"
+st1 = "not"
+st2="years old"
+st_f="I am not {} years old"
+st_not_f="not"
+print(st_f.format(st_not_f,age))
+print(st, age, st2)
+print(st_f)
+
+#user Input
+
+age1= input("please emter yopur age")
+print(age1)
+print(type(age1))
+
+print("In 1 year, you willbe {} years old")
